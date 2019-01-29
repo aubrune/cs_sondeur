cc4c11f81bb6b31ef147a705efef068e79e371a5
Agathe Lagache <agathe.lagache@viacesi.fr>
2019-01-29 09:41:14 +0100

    Modification of the board + Add moveit.rviz

--- src/sondeur.py
+++ src/sondeur.py
@@ -5,15 +5,17 @@ import rospy, tf2_ros
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Quaternion, Pose, Point, Vector3, TransformStamped
 from std_msgs.msg import Header, ColorRGBA
+from tf.transformations import quaternion_from_euler
 
 if __name__=='__main__':
     rospy.init_node('sondeur')
     rospy.sleep(1)
     br = tf2_ros.TransformBroadcaster()
     t = TransformStamped()
-
-    x_table, y_table, z_table = 0.5, 0.5, 1.45
-    qx_table, qy_table, qz_table, qw_table = 0, 0, 0, 1
+    quartenion = quaternion_from_euler(0, 0.05, 0)
+    
+    x_table, y_table, z_table = 0.50, 0, 0.75
+    qx_table, qy_table, qz_table, qw_table = quartenion[0], quartenion[1], quartenion[2], quartenion[3]
     t.header.frame_id = "world"
     t.child_frame_id = "table"
     t.transform.translation.x = x_table
@@ -26,7 +28,7 @@ if __name__=='__main__':
 
     
     marker_publisher = rospy.Publisher('visualization_marker', Marker, queue_size=5)
-    table = Marker(type=Marker.CUBE, color=ColorRGBA(0.0,1.0,0.0,0.8), pose=Pose(Point(x_table, y_table, z_table), Quaternion(qx_table, qy_table, qz_table, qw_table)), scale=Vector3(1, 1, 1), header=Header(frame_id='world'))
+    table = Marker(type=Marker.CUBE, color=ColorRGBA(255,255,255,0.8), pose=Pose(Point(x_table, y_table, z_table), Quaternion(qx_table, qy_table, qz_table, qw_table)), scale=Vector3(0.01, 1.50, 1.50), header=Header(frame_id='world'))
     while not rospy.is_shutdown():
         marker_publisher.publish(table)
         t.header.stamp = rospy.Time.now()
