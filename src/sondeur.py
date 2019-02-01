8ed844eff341cc016f14f606060cb8e982b6d185
Agathe Lagache <agathe.lagache@viacesi.fr>
2019-02-01 11:41:07 +0100

    Add comments

--- src/sondeur.py
+++ src/sondeur.py
@@ -7,22 +7,23 @@ from geometry_msgs.msg import Quaternion, Pose, Point, Vector3, TransformStamped
 from std_msgs.msg import Header, ColorRGBA
 from tf.transformations import quaternion_from_euler
 import moveit_commander
+import moveit_msgs.msg
+
+import copy
 
 if __name__=='__main__':
-    rospy.init_node('sondeur')
-    rospy.sleep(1)
-    br = tf2_ros.TransformBroadcaster()
-    t = TransformStamped()
+    rospy.init_node('sondeur')          #Initialisation du node 'sondeur'
+    rospy.sleep(1)                      
     
+    br = tf2_ros.TransformBroadcaster() #Définit l'emetteur de données
+    t = TransformStamped()              #Permet la transformation du parent à l'enfant    
     fear = TransformStamped()
+    
+    x_table, y_table, z_table = 0.40, 0, 0.75   #Diverses variables pour le marker table
     quartenion = quaternion_from_euler(0, 0.05, 0)
-    
-    group_name = "edo"
-    group = moveit_commander.MoveGroupCommander(group_name)
-    
-    x_table, y_table, z_table = 0.40, 0, 0.75
     qx_table, qy_table, qz_table, qw_table = quartenion[0], quartenion[1], quartenion[2], quartenion[3]
-    t.header.frame_id = "world"
+    
+    t.header.frame_id = "world"                 #Repère tableau
     t.child_frame_id = "table"
     t.transform.translation.x = x_table
     t.transform.translation.y = y_table
@@ -32,7 +33,7 @@ if __name__=='__main__':
     t.transform.rotation.z = qz_table
     t.transform.rotation.w = qw_table
     
-    fear.header.frame_id = "table"
+    fear.header.frame_id = "table"              #Repère début de la colonne peur
     fear.child_frame_id = "fear_start"
     fear.transform.translation.x = 0
     fear.transform.translation.y = 0.5
@@ -41,15 +42,23 @@ if __name__=='__main__':
     fear.transform.rotation.y = 0
     fear.transform.rotation.z = 0
     fear.transform.rotation.w = 1
-
     
-    marker_publisher = rospy.Publisher('visualization_marker', Marker, queue_size=5)
-    table = Marker(type=Marker.CUBE, color=ColorRGBA(255,255,255,0.8), pose=Pose(Point(x_table, y_table, z_table), Quaternion(qx_table, qy_table, qz_table, qw_table)), scale=Vector3(0.01, 1.50, 1.50), header=Header(frame_id='world'))
-    while not rospy.is_shutdown():
-        marker_publisher.publish(table)
-        t.header.stamp = rospy.Time.now()
-        br.sendTransform(t)
+    marker_publisher = rospy.Publisher('visualization_marker',       #Publication topic visualization_marker 
+                                        Marker, 
+                                        queue_size=5)           
+    table = Marker(type=Marker.CUBE,                                        #Forme du marker    #Définition du marker
+                    color=ColorRGBA(255,255,255,0.8),                       #Couleur
+                    pose=Pose(Point(x_table, y_table, z_table),             #Position
+                    Quaternion(qx_table, qy_table, qz_table, qw_table)),    #Rotation
+                    scale=Vector3(0.01, 1.50, 1.50),                        #Taille
+                    header=Header(frame_id='world'))                        #Parent
+    
+    while not rospy.is_shutdown():              #Tant que le processus n'est pas quitté
+        marker_publisher.publish(table)         #Publication du marker table
+        t.header.stamp = rospy.Time.now()       #Définit le temps de l'entête
+        br.sendTransform(t)                     #Envoie la transformation de l'enfant au parent sur le topic ROS
         fear.header.stamp = rospy.Time.now()
         br.sendTransform(fear)
         print("Marker published!")
         rospy.sleep(1)
+
